0aeec76a354b8e510d9b16b67a1bafa36d78c514
Guiznn013 <guariniellolives@gmail.com>
2025-06-10 02:32:08 -0300

    script.js modified

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function generateNumber(){
-    const min = Math.ceil(document.querySelector(".input-min").value);
-    const max = Math.floor(document.querySelector(".input-max").value);
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-    alert(`Numero Gerado: ${result}`);
+    if(min < max){
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert(`Numero Gerado: ${result}`);
+    }else {
+        alert("O valor mínimo tem que ser menor que o valor máximo.")
+    }
 }
